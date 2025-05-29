1425a5df6d124da88cb5813be3fd948bd5d53fc6
Yamato Kamei <s2015@oita.kosen-ac.jp>
2025-05-30 03:16:40 +0900

    WIP senderの整備

--- include/roboclaw_ros2/sender.hpp
+++ include/roboclaw_ros2/sender.hpp
@@ -15,15 +15,17 @@ class Sender {
 public:
   Sender(std::shared_ptr<SerialPort> device);
   ~Sender();
-  bool send(const std::vector<uint8_t> & data, size_t length);
+  bool Sender::send(std::vector<uint8_t> && data);
+
+  bool drive_forward_M1(uint8_t address, uint8_t value);
 
 private:
   std::shared_ptr<SerialPort> device_;
 
-  bool setBaudRate(int baudrate);
-  bool configurePort();
+  uint16_t crc16(const std::vector<uint8_t> & data);
   void handleError(const std::string & message);
 
+  bool compatibility_commands(uint8_t address, uint8_t command, uint8_t byte_value)
 };
 
 /*
--- src/sender.cpp
+++ src/sender.cpp
@@ -12,3 +12,60 @@ Sender::Sender(std::shared_ptr<SerialPort> device)
 Sender::~Sender()
 {
 }
+
+bool Sender::send(std::vector<uint8_t> && data)
+{
+  if (!device_ || !device_->isOpen()) {
+    handleError("Serial port is not open or invalid.");
+    return false;
+  }
+
+  uint16_t crc = crc16(data);
+  data.push_back(static_cast<uint8_t>(crc >> 8));
+  data.push_back(static_cast<uint8_t>(crc & 0xFF));
+
+  try {
+    device_->write(std::move(data));
+  } catch (const std::runtime_error & e) {
+    handleError(e.what());
+    return false;
+  }
+
+  return true;
+}
+
+bool Sender::drive_forward_M1(uint8_t address, uint8_t value)
+{
+  return this->compatibility_commands(address, 0x00, value);
+}
+
+uint16_t Sender::crc16(const std::vector<uint8_t> & data)
+{
+  uint16_t crc = 0; // Initial value
+  for (int byte = 0; byte < data.size(); byte++) {
+    uint16_t crc = crc ^ ((unsigned int)data[byte] << 8);
+    for (unsigned char bit = 0; bit < 8; bit++) {
+      if (crc & 0x8000) {
+        crc = (crc << 1) ^ 0x1021;
+      } else {
+        crc = crc << 1;
+      }
+    }
+  }
+  return crc & 0xFFFF;
+}
+
+bool Sender::compatibility_commands(uint8_t address, uint8_t command, uint8_t byte_value)
+{
+
+  std::vector<uint8_t> data(5);
+  data.push_back(address);
+  data.push_back(command);
+  data.push_back(byte_value);
+
+  uint16_t crc = crc16(data);
+  data.push_back(static_cast<uint8_t>(crc >> 8));
+  data.push_back(static_cast<uint8_t>(crc & 0xFF));
+
+  return send(std::move(data));
+}
