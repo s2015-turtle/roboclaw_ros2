74fdd2206cfdcb59487dfab942566400d58e067f
Yamato Kamei <s2015@oita.kosen-ac.jp>
2025-06-10 22:34:06 +0900

    roboclaw wip

---
+++ include/roboclaw_ros2/roboclaw.hpp
@@ -0,0 +1,16 @@
+#ifndef ROBOCLAW_ROS2_ROBOCLAW_HPP
+#define ROBOCLAW_ROS2_ROBOCLAW_HPP
+
+#include "roboclaw_ros2/serial_port.hpp"
+
+namespace roboclaw_ros2
+{
+class Roboclaw {
+private:
+  
+
+public:
+  
+};
+}
+#endif // ROBOCLAW_ROS2_ROBOCLAW_HPP
