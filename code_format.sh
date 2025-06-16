68a39a1ad2384cfc92d7aaac74372608fe82d3c4
s2015-turtle <ykturtler@outlook.com>
2025-06-16 19:43:01 +0900

    fix

--- code_format.sh
+++ code_format.sh
@@ -1,4 +1,4 @@
 # !/bin/sh
-ament_uncrustify --reformat include/
+ament_uncrustify --reformat include/roboclaw_ros2/
 ament_uncrustify --reformat src/
 echo "Code formatting completed using ament_uncrustify."
